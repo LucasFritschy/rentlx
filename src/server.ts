657c437fa7f1df9ef0a1066adb59122df3afbcdb
LucasFritschy <lucasfritschy@gmail.com>
2022-01-18 15:39:52 -0300

    add debug configuration

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,13 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (request, response) => response.json({ message: 'hello bacana' }))
+app.use(express.json())
 
-app.listen(3333)
+app.get('/', (request, response) => response.json({ message: 'hello world' }))
+
+app.post('/courses', (request, response) => {
+	const { name } = request.body
+	return response.json({ name })
+})
+
+app.listen(3333, () => console.log('server is running'))

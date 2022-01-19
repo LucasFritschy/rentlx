f68d775edf091d888aab6efa3ae1b6b8b50f7298
LucasFritschy <lucasfritschy@gmail.com>
2022-01-19 20:45:43 -0300

    checking if category exists before category creation

--- src/repositories/CategoryRepository.ts
+++ src/repositories/CategoryRepository.ts
@@ -12,9 +12,17 @@ class CategoryRepository {
 		this.categories = []
 	}
 
-	create({ name, description }: ICategoryDTO) {
+	list(): Category[] {
+		return this.categories
+	}
+
+	create({ name, description }: ICategoryDTO): void {
 		const category = new Category(name, description)
 		this.categories.push(category)
+	}
+
+	findByName(name: string): Category | undefined {
+		const category = this.categories.find(category => category.name === name)
 
 		return category
 	}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -6,9 +6,21 @@ const categoriesRoutes = Router()
 
 const categoriesRepository = new CategoryRepository()
 
+categoriesRoutes.get('/', (request, response) => {
+	const categories = categoriesRepository.list()
+
+	return response.status(200).json({ categories })
+})
+
 categoriesRoutes.post('/', (request, response) => {
 
 	const { name, description } = request.body
+
+	const categoryAlreadyExists = categoriesRepository.findByName(name)
+
+	if(categoryAlreadyExists) {
+		return response.status(400).json({ error: 'Categoria já cadastrada no sistema'})
+	}
 
 	const category = categoriesRepository.create({ name, description })
 

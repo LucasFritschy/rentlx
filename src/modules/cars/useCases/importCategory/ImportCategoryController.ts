de1a83d6b5c0261f64ce9bb0851e9a4d16862fe8
LucasFritschy <lucasfritschy@gmail.com>
2022-01-25 20:37:09 -0300

    saving categories on database with upload

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -5,12 +5,14 @@ import { ImportCategoryUseCase } from './ImportCategoryUseCase'
 class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
 
-  handle(request: Request, response: Response) {
+  handle(request: Request, response: Response): Response {
     const { file } = request
 
-    this.importCategoryUseCase.execute(file)
-
-    return response.json({ message: 'ok' })
+    if (file) {
+      this.importCategoryUseCase.execute(file)
+      return response.json({ message: 'ok' })
+    }
+    return response.json({ error: 'something went wrong' })
   }
 }
 
